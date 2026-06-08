991e4f9a24c923c232c4b8edf895946f7ab72d17
akash-code-org <akash.workspace@outlook.com>
2026-06-09 03:53:08 +0500

    vowel and const checker

---
+++ vowel.py
@@ -0,0 +1,5 @@
+n = input("Enter the character: ")
+if n == 'a' or n == 'i' or n == 'o' or n == 'u' or n == 'e' or n == 'A' or n == 'I' or n == 'O' or n == 'U' or n == 'E':
+    print("Vowel: ")
+else:
+    print("Consonant: ")
