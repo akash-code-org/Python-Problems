a1841fff24ef49090ce57088f4cd77e444093248
akash-code-org <akash.workspace@outlook.com>
2026-06-09 01:52:03 +0500

    Updated

--- tempreture.py
+++ tempreture.py
@@ -1,7 +1,9 @@
 temp = int(input("Enter tempreture: "))
-if temp > 1 and temp < 30:
-    print("Tempreture is normal: ")
-elif temp > 30 and temp < 60:
+if temp < 0 and temp > -1:
+    print("Tempreture is cold: ")
+elif temp > 1 and temp < 30:
+        print("Tempreture is normal: ")
+elif temp > 30:
     print("Tempreture is hot: ")
 else:
     print("Tempreture is cold: ")
