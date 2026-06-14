29608604d46b03737d3764aecbe02872680ab256
akash-code-org <akash.workspace@outlook.com>
2026-06-14 15:49:08 +0500

    and operator used

---
+++ passFail.py
@@ -0,0 +1,7 @@
+eng = int(input("Enter english marks: "))
+math = int(input("Enter math marks: "))
+phy = int(input("Enter physics marks: "))
+if eng >= 50 and math >= 50 and phy >= 50:
+    print("Over all pass: ")
+else:
+    print("Fail:")
