1707d1767c3a6e3373401b3e7e92644d78d0888b
akash-code-org <akash.workspace@outlook.com>
2026-06-15 20:58:36 +0500

    M

---
+++ Relat_Operators.py
@@ -0,0 +1,10 @@
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+if first > second:
+    print("First is greater than the second: ")
+elif first == second:
+    print("Both are equal: ")
+elif first != second:
+    print("Both are not equal: ")
+else:
+    print("First is less than second: ")
