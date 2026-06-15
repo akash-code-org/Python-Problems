b8cd0a38fe138bf2c676f6c0b34182c6993bfad5
akash-code-org <akash.workspace@outlook.com>
2026-06-15 20:18:54 +0500

    M

---
+++ m_i-Operators.py
@@ -0,0 +1,5 @@
+b = [1,2,3,4,5]
+if 2 in b:
+    print("present: ")
+else:
+    print("not")
---
+++ table.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number: "))
+for i in range(1,10,):
+    print(n,"x",i,"=",i*n)
